a8cae234528b09a3e5262095cf685863d5c3c2cd
Clam- <clam@nyanya.org>
2023-08-20 04:33:52 +1000

    Update example.py
    Added some debug code

--- example.py
+++ example.py
@@ -3,35 +3,35 @@ import time
 from dual_mc33926 import motors, MAX_SPEED
 
 # Set up sequences of motor speeds.
-test_forward_speeds = list(range(0, MAX_SPEED, 10)) + \
-  [MAX_SPEED] * 200 + list(range(MAX_SPEED, 0, -10)) + [0]
+test_forward_speeds = list(range(0, MAX_SPEED, 1000)) + \
+  [MAX_SPEED] * 200 + list(range(MAX_SPEED, 0, -1000)) + [0]
 
-test_reverse_speeds = list(range(0, -MAX_SPEED, -10)) + \
-  [-MAX_SPEED] * 200 + list(range(-MAX_SPEED, 0, 10)) + [0]
+test_reverse_speeds = list(range(0, -MAX_SPEED, -1000)) + \
+  [-MAX_SPEED] * 200 + list(range(-MAX_SPEED, 0, 1000)) + [0]
 
 try:
     motors.enable()
     motors.setSpeeds(0, 0)
 
-    print("Motor 1 forward")
+    print("\nMotor 1 forward: ")
     for s in test_forward_speeds:
         motors.motor1.setSpeed(s)
-        time.sleep(0.005)
+        print(s, end="")
 
-    print("Motor 1 reverse")
+    print("\nMotor 1 reverse: ")
     for s in test_reverse_speeds:
         motors.motor1.setSpeed(s)
-        time.sleep(0.005)
+        print(s, end="")
 
-    print("Motor 2 forward")
+    print("\nMotor 2 forward: ")
     for s in test_forward_speeds:
         motors.motor2.setSpeed(s)
-        time.sleep(0.005)
+        print(s, end="")
 
-    print("Motor 2 reverse")
+    print("\nMotor 2 reverse: ")
     for s in test_reverse_speeds:
         motors.motor2.setSpeed(s)
-        time.sleep(0.005)
+        print(s, end="")
 
 finally:
   # Stop the motors, even if there is an exception
